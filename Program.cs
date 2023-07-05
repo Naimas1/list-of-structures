202afa65d41e453b2ac23e92d1fc685deca8a040
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-05 11:41:44 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace list_of_structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
