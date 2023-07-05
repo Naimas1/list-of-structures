2cb87494fe50f2c92f8e02041b98d2aa91052f33
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-05 12:05:10 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,60 @@
-﻿namespace list_of_structures
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_of_structures
 {
-    internal class Program
+    struct Birthday
     {
-        static void Main(string[] args)
+        private DateTime dateOfBirth;
+
+        public void SetDateOfBirth(int year, int month, int day)
         {
-            Console.WriteLine("Hello, World!");
+            dateOfBirth = new DateTime(year, month, day);
+        }
+
+        public string GetDayOfWeekOfBirth()
+        {
+            return dateOfBirth.DayOfWeek.ToString();
+        }
+
+        public string GetDayOfWeekInYear(int year)
+        {
+            DateTime birthdayInYear = new DateTime(year, dateOfBirth.Month, dateOfBirth.Day);
+            return birthdayInYear.DayOfWeek.ToString();
+        }
+
+        public int GetDaysUntilBirthday()
+        {
+            DateTime today = DateTime.Today;
+            DateTime nextBirthday = new DateTime(today.Year, dateOfBirth.Month, dateOfBirth.Day);
+
+            if (nextBirthday < today)
+                nextBirthday = nextBirthday.AddYears(1);
+
+            return (nextBirthday - today).Days;
+        }
+    }
+
+    class Programdate
+    {
+        public static void Main()
+        {
+            Birthday birthday = new Birthday();
+
+           
+            birthday.SetDateOfBirth(2000, 4, 1);
+
+            string dayOfWeekOfBirth = birthday.GetDayOfWeekOfBirth();
+            Console.WriteLine($"День народження припадає на {dayOfWeekOfBirth}");
+
+            string dayOfWeekInYear = birthday.GetDayOfWeekInYear(2025);
+            Console.WriteLine($"У 2025 році день народження припаде на {dayOfWeekInYear}");
+
+            int daysUntilBirthday = birthday.GetDaysUntilBirthday();
+            Console.WriteLine($"До дня народження залишилося {daysUntilBirthday} днів");
         }
     }
 }
